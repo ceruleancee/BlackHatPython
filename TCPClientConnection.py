2d829487f6bb1dba0973112e23839b441f2d4609
ceruleancee <ceruleancee@gmail.com>
2019-04-10 19:58:01 -0500

    Update TcpClientConnection

--- TCPClientConnection.py
+++ TCPClientConnection.py
@@ -10,9 +10,9 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
 
 # send a get request
-client.send("GET /HTTP/1.1/\r\nHost: google.com\r\n\r\n")
+client.send("GET / HTTP/1.1/\r\nHost:google.com\r\n\r\n")
 
 # receive on designated port
 response = client.recv(4096)
 
-print(response)
+print response
