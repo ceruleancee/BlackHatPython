157b1957ecaf3de2b9be71cc6b73e9f40735027a
ceruleancee <ceruleancee@gmail.com>
2020-02-18 19:26:04 -0600

    Remove comments

--- TCPClientConnection.py
+++ TCPClientConnection.py
@@ -6,16 +6,12 @@ import socket
 target_host = "www.google.com"
 target_port = 80
 
-# create socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# connect client
 client.connect((target_host, target_port))
 
-# send a get request
 client.send("GET / HTTP/1.1/\r\nHost:google.com\r\n\r\n")
 
-# receive on designated port
 response = client.recv(4096)
 
 print response
--- TCPServer.py
+++ TCPServer.py
@@ -10,19 +10,15 @@ bind_port = 9999
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((bind_ip,bind_port))
 
-# max backlog connections set to 5
 server.listen(5)
 
 print "[*] Listen on %s:%d" % (bind_ip,bind_port)
 
-# client-handling thread
 def handle_client(client_socket):
 
-    # print whats the client sends
     request = client_socket.recv(1024)
     print "[*] Received: %s" % request
 
-    #send back a packet
     client_socket.send("ACK! Chocolate, chocolate, chocolate!")
     client_socket.close()
 
@@ -31,6 +27,5 @@ while True:
     client,addr = server.accept()
     print "[*] Accepted connection from: %s:%d" % (addr[0], addr[1])
 
-    # start up client thread to handle incoming data
     client_handler = threading.Thread(target=handle_client,args=(client,))
     client_handler.start()
