a6930b3dab7af4e8de4146415ebc1e72022f973e
Claudia Ortiz <claudia@graylog.com>
2020-08-25 19:50:18 -0500

    Add content_bruter

---
+++ ch05/content_bruter.py
@@ -0,0 +1,32 @@
+import urllib2
+import threading
+import Queue
+import urllib
+
+threads = 50
+target_url = "http://testphp.vulnweb.com"
+wordlist_file = "tmp/all.txt"  # from SVNDigger
+resume = None
+user_agent = "Mozilla/5.0 (X11; Linux x86_64; rv:19.0) Gecko/20100101 Firefox/19.0"
+
+
+def build_wordlist():
+    fd = open(wordlist_file, "rb")
+    raw_words = fd.readlines()
+    fd.close()
+
+    found_resume = False
+    words = Queue.Queue()
+
+    for word in raw_words:
+        word = word.rstrip()
+        if resume is not None:
+            if found_resume:
+                words.put(word)
+            else:
+                if word == resume:
+                    found_resume = True
+                    print("Resuming wordlist from : %s" % resume)
+        else:
+            words.put(word)
+    return words
