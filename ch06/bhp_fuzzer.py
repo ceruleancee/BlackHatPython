38e1b94b35a5ac689e24c38ce42ab688d0fc5483
Claudia Ortiz <claudia@graylog.com>
2020-09-15 20:00:50 -0500

    Add bhp_fuzzer

---
+++ ch06/bhp_fuzzer.py
@@ -0,0 +1,22 @@
+from burp import IBurpExtender
+from burp import IIntruderPayloadGeneratorFactory
+from burp import IIntruderPayloadGenerator
+
+from java.util import List, ArrayList
+
+import random
+
+class BurpExtender(IBurpExtender, IIntruderPayloadGeneratorFactory):
+    def registerExtenderCallbacks(self, callbacks):
+        self._callbacks = callbacks
+        self._helpers = callbacks.getHelpers()
+
+        callbacks.registerIntruderPayloadGeneratorFactory(self)
+
+        return
+
+    def getGenerator(self):
+        return "BHP Payload Generator"
+
+    def createNewInstance(self, attack):
+        return BHPFuzzer(self, attack)
